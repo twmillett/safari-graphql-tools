7157d0340905620a4d13eb89a901d432bb247582
Tom Millett <tmillett@microsoft.com>
2020-09-15 06:24:27 -0700

    Updates

---
+++ precache-manifest.a577d7573078d7d87b796718a030cc5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b5a4ecfd0f17dbaa0d4a1016565ad3",
+    "url": "/twmillett/safari-graphql-tools/index.html"
+  },
+  {
+    "revision": "485f60f650d58e914ff4",
+    "url": "/twmillett/safari-graphql-tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6be6cebce31c474b6ba2",
+    "url": "/twmillett/safari-graphql-tools/static/js/2.37d487bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/twmillett/safari-graphql-tools/static/js/2.37d487bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485f60f650d58e914ff4",
+    "url": "/twmillett/safari-graphql-tools/static/js/main.d604a4e9.chunk.js"
+  },
+  {
+    "revision": "d01b7e3f0f23a4adb070",
+    "url": "/twmillett/safari-graphql-tools/static/js/runtime-main.9ff9c0d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/twmillett/safari-graphql-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
